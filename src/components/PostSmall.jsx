c7c835911b4ad2b5b0dcf6e9bf237c1b8f9b7d57
vitto2 <vitor2071@gmail.com>
2025-05-06 16:41:17 -0300

    feat: ajustando quebra de elementos no card small

--- src/components/PostSmall.jsx
+++ src/components/PostSmall.jsx
@@ -15,11 +15,13 @@ export default function PostSmall({ autor, titulo, data, descricao, clamped }) {
 	}
 	return (
 		<section className="min-h-[11.75rem] min-w-[148px] md:max-w-[34.25rem] xl:max-w-[34.25rem] xl:max-h-[36.37rem] border border-[#2500FF] rounded-xl px-[.75rem] py-[.5rem] xl:px-[1.5rem] xl:py-[1rem] md:px-[24px] md:py-[16px]">
-			<div className="pb-3 flex justify-between items-start text-[#2500FF] font-semibold text-[0.625rem]">
-				<div className="flex flex-wrap items-center gap-1 leading-tight font-normal md:text-[.875rem]">
-					<span className="whitespace-nowrap">{autor}</span>
-					<span className="mx-[1px]">•</span>
-					<span className="whitespace-nowrap">{formatarData(data)}</span>
+			<div className="pb-3 flex  justify-between items-start text-[#2500FF] font-semibold text-[0.625rem]">
+				<div className="line-clamp-2 overflow-hidden flex flex-wrap items-center gap-1 leading-tight font-normal md:text-[.875rem]">
+					<div className="w-[140px]">
+						<span className="whitespace-nowrap">{autor}</span>
+						<span className="mx-[1px]">•</span>
+						<span className="whitespace-nowrap">{formatarData(data)}</span>
+					</div>
 				</div>
 				<img src={arrow} alt="seta" className="w-3 h-3 mt-[2px]" />
 			</div>
